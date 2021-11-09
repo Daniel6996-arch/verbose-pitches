8ac43b4e983b1f68a43df4dad443627bf3f46c60
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-11-09 07:21:05 +0300

    add user functionality

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
+
 class Pitch:
     '''
     Pitch class to define Pitch Objects
@@ -9,3 +13,37 @@ class Pitch:
         self.upvote = upvote
         self.downvote = downvote
         
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    pass_secure = db.Column(db.String(255))
+
+    @property
+        def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+        @password.setter
+        def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+
+    def __repr__(self):
+        return f'User {self.username}'
